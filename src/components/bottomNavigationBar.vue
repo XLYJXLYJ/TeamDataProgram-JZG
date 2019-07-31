0856946d1ed00c5ae7c584ca1cc875246fdff1bf
XLYJXLYJ <18050435245@163.com>
2019-07-31 17:46:10 +0800

    2019.7.31 17:46 星期三

--- src/components/bottomNavigationBar.vue
+++ src/components/bottomNavigationBar.vue
@@ -15,7 +15,7 @@
                 </li>
             </ul>
         </article>
-        <img class="buttom-img" src="/static/images/button.png">
+        <img class="buttom-img" src="/static/images/button.png" @click="getUser">
     </section>
 </template>
 <script>
@@ -62,6 +62,21 @@ export default {
             wx.reLaunch({
                 url
             });
+        },
+        getUser(){
+            wx.getSetting({
+                success(res) {
+                    if (!res.authSetting['scope.record']) {
+                    wx.authorize({
+                        scope: 'scope.record',
+                        success () {
+                        // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+                        wx.startRecord()
+                        }
+                    })
+                    }
+                }
+            })
         }
     }
 };
